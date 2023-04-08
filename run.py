be7746a2feda79232fd3ad52f6c7f8da1aa3eea8
satogako <genevaq@ukr.net>
2023-04-08 19:19:40 +0000

    Retrieve latest stock numbers from spreadsheet

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -12,9 +13,13 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
+
 def get_sales_data():
     """
     Get sales figures input from the user.
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a string of 6 numbers separated
+    by commas. The loop will repeatedly request data, until it is valid.
     """
     while True:
         print("Please enter sales data from the last market.")
@@ -22,43 +27,68 @@ def get_sales_data():
         print("Example: 10,20,30,40,50,60\n")
 
         data_str = input("Enter your data here: ")
-        
-        sales_data = data_str.split(',')
-       
+
+        sales_data = data_str.split(",")
+
         if validate_data(sales_data):
-            print('Data is Valid!')
+            print("Data is valid!")
             break
+
     return sales_data
 
+
 def validate_data(values):
-    '''
+    """
     Inside the try, converts all string values into integers.
     Raises ValueError if strings cannot be converted into int,
     or if there aren't exactly 6 values.
-    print(values)
-    '''
+    """
     try:
         [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
-                f'Exactly 6 values requaired, you provided {len(values)}'
+                f"Exactly 6 values required, you provided {len(values)}"
             )
     except ValueError as e:
-            print(f'Invalid data: {e}, please try againe.\n')
-            return False
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
+
     return True
 
-def update_sales_worksheet(sales_data):
+
+def update_sales_worksheet(data):
     """
     Update sales worksheet, add new row with the list data provided
     """
     print("Updating sales worksheet...\n")
-    # методом worksheet() отримуэмо доступ до вкладки "sales" у вкладці google таблиці  Love_Sandwiches
-    sales_worksheet = SHEET.worksheet("sales") 
-    #Метод append_row додає новий рядок у кінець наших даних на вибраному аркуші
+    sales_worksheet = SHEET.worksheet("sales")
     sales_worksheet.append_row(data)
     print("Sales worksheet updated successfully.\n")
 
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
+
+def calculate_surplus_data(sales_row):
+    """
+    Compare sales with stock and calculate the surplus for each item type.
+    The surplus is defined as the sales figure subtracted from the stock:
+    - Positive surplus indicates waste
+    - Negative surplus indicates extra made when stock was sold out.
+    """
+    print("Calculating surplus data...\n")
+    stock = SHEET.worksheet("stock").get_all_values() #отримує доступ до вкладки 'stock' та до всіх даних на ній
+    stock_row = stock[-1] #виведе останній рядок з таблиці
+    print(stock_row)
+    # pprint(stock) виведе кожен список в окремому рядку, а також потрібно добавити до import на початку
+
+
+def main():
+    """
+    Run all program functions
+    """
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    calculate_surplus_data(sales_data)
+
+
+print("Welcome to Love Sandwiches Data Automation")
+main()
